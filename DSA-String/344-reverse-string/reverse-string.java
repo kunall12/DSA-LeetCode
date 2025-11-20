fe446edb965c1e3a192bf58c4a16cf048919e490
kunal prasad yadav <kunalyaduvanshi2405@gmail.com>
2025-11-20 23:51:09 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (79.52%) - LeetSync

---
+++ DSA-String/344-reverse-string/reverse-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    //Reverse String
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            // swap characters
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
