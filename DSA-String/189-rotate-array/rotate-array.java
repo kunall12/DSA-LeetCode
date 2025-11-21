b2c5961437fcc46e15a384e4fd581c33d9e62429
kunal prasad yadav <kunalyaduvanshi2405@gmail.com>
2025-11-21 21:19:08 +0530

    Time: 0 ms (100.00%) | Memory: 60.8 MB (27.20%) - LeetSync

---
+++ DSA-String/189-rotate-array/rotate-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    //  Rotate Array
+    public void rotate(int[] arr,int k) {
+        k=k%arr.length;
+        reverseArray(arr,0,arr.length-1);
+        reverseArray(arr,0,k-1);
+        reverseArray(arr,k,arr.length-1);
+
+    }
+    public void reverseArray(int[] arr, int start, int end){
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+
+            start++;
+            end--;
+        }
+    }
+}
