15884a5343caab111c115844169e501497dc4553
kunal prasad yadav <kunalyaduvanshi2405@gmail.com>
2025-11-21 22:49:51 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (12.75%) - LeetSync

---
+++ Array/665-non-decreasing-array/non-decreasing-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int count = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1]) {
+                count++;
+                if (count > 1) return false;
+
+                if (i == 1 || nums[i - 2] <= nums[i]) {
+                    nums[i - 1] = nums[i];   // fix left
+                } else {
+                    nums[i] = nums[i - 1];   // fix right
+                }
+            }
+        }
+        return true;
+    }
+}
